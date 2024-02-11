6de195e8e9f59542d9a12659c36d0b89a45df4a7
Ali Al Oreabi <a.aloreabi2000@gmail.com>
2024-02-11 15:24:37 -0500

    added styles.js

--- components/loginScreen.js
+++ components/loginScreen.js
@@ -1,5 +1,8 @@
+
 import React, {useState} from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
+import styles from './styles';
+
 import {
   SafeAreaView,
   KeyboardAvoidingView,
@@ -7,7 +10,6 @@ import {
   TextInput,
   Text,
   TouchableOpacity,
-  StyleSheet,
 } from 'react-native';
 
 const LoginScreen = () => {
@@ -54,90 +56,5 @@ const LoginScreen = () => {
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  gradient: {
-    flex: 1,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#7D3C98', // Set your desired color
-  },
-  keyboardView: {
-    flex: 1,
-  },
-  loginContainer: {
-    margin: 20,
-    padding: 20,
-    backgroundColor: '#FFFFFF40', // Adjust transparency as needed
-    borderRadius: 10,
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#FFF',
-    alignItems: 'center',
-    margin: 20,
-    marginLeft: 75
-  },
-  loginTitle:{
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#FFF',
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  input: {
-    width: '100%',
-    backgroundColor: '#FFFFFF10', // Adjust transparency as needed
-    padding: 15,
-    borderRadius: 5,
-    marginVertical: 20,
-
-  },
-  loginButton: {
-    width: '100%',
-    padding: 15,
-    backgroundColor: '#8E44AD', // Adjust the color as needed
-    borderRadius: 5,
-    alignItems: 'center',
-    marginTop: 20,
-  },
-  loginButtonText: {
-    color: '#FFF',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  optionsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-    margin: 25,
-  },
-  checkboxContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  checkbox: {
-    width: 20,
-    height: 20,
-    marginRight: 8,
-    borderWidth: 1,
-    borderColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  checkboxText: {
-    color: '#FFF',
-  },
-  rememberMeText: {
-    color: '#FFF',
-  },
-  forgotPasswordText: {
-    color: '#FFF',
-    textDecorationLine: 'underline'
-  }
-});
 
 export default LoginScreen;
---
+++ components/styles.js
@@ -0,0 +1,104 @@
+// styles.js
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+
+    gradient: {
+        flex: 1,
+      },
+      container: {
+        flex: 1,
+        backgroundColor: '#7D3C98', 
+      },
+      keyboardView: {
+        flex: 1,
+      },
+      loginContainer: {
+        margin: 20,
+        padding: 20,
+        backgroundColor: '#FFFFFF40', 
+        borderRadius: 10,
+        alignItems: 'center',
+      },
+      title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#FFF',
+        alignItems: 'center',
+        margin: 20,
+        marginLeft: 75
+      },
+      loginTitle:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#FFF',
+        alignItems: 'center',
+        marginBottom: 20,
+      },
+      input: {
+        width: '100%',
+        backgroundColor: '#FFFFFF10', 
+        padding: 15,
+        borderRadius: 5,
+        marginVertical: 20,
+    
+      },
+      loginButton: {
+        width: '100%',
+        padding: 15,
+        backgroundColor: '#8E44AD', 
+        borderRadius: 5,
+        alignItems: 'center',
+        marginTop: 20,
+      },
+      loginButtonText: {
+        color: '#FFF',
+        fontSize: 18,
+        fontWeight: 'bold',
+      },
+      optionsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        margin: 25,
+      },
+      checkboxContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+      checkbox: {
+        width: 20,
+        height: 20,
+        marginRight: 8,
+        borderWidth: 1,
+        borderColor: '#FFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      checkboxText: {
+        color: '#FFF',
+      },
+      rememberMeText: {
+        color: '#FFF',
+      },
+      forgotPasswordText: {
+        color: '#FFF',
+        textDecorationLine: 'underline'
+      }
+//   loginScreen: {
+//     // ... your styles for login screen
+//   },
+//   dashboardScreen: {
+//     // ... your styles for dashboard screen
+//   },
+//   settingsScreen: {
+//     // ... your styles for settings screen
+//   },
+//   aboutScreen: {
+//     // ... your styles for about screen
+//   },
+//   faqScreen: {
+//     // ... your styles for FAQ screen
+//   },
+//   // Add more component styles as needed
+});
