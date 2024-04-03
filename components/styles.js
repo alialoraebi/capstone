fecdfdbc83920a1955b3837de54b51a5a661ad64
Alvaro <alvaro.aguirremeza@georgebrown.ca>
2024-04-02 22:03:53 -0400

    fixed some styling

--- components/styles.js
+++ components/styles.js
@@ -370,19 +370,19 @@ export default StyleSheet.create({
       paddingTop: 20,
       fontSize: 30,
       fontWeight: 'bold',
-      color: '#600060', 
+      color: 'Black', 
       alignSelf: 'center',
       marginVertical: 10,
     },
     FAQheader: {
       fontSize: 24,
       fontWeight: 'bold',
-      color: '#600060', 
+      color: 'Black', 
       marginBottom: 10,
     },
     FAQsubHeader: {
       fontSize: 16,
-      color: '#600060', 
+      color: 'Black', 
       marginBottom: 20,
     },
     searchContainer: {
@@ -402,6 +402,7 @@ export default StyleSheet.create({
     },
     faqContainer: {
       marginBottom: 20,  
+      backgroundColor: '#FFFFFF',
     },
     faqItem: {
       marginBottom: 20, 
